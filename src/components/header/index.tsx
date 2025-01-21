986f1e0a654adf9ac5242b9bb6676964ef7ffb28
BTFitti <bruno.fitti734@live.com>
2025-01-21 15:55:23 -0300

    Added the api

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,12 +2,17 @@ import { Link } from "react-router-dom";
 import { FiShoppingCart } from "react-icons/fi";
 export function Header() {
   return (
-    <header className="w-full bg-orange-400 p-1">
-      <nav className="max-w-7xl flex items-center justify-between px-5 mx-auto h-20">
-        <Link to={"/"} className="text-5xl font-bold text-purple-900">
+    <header className="w-full bg-orange-400 bg-gradient-to-r from-slate-800 p-1">
+      <nav className="max-w-7xl flex items-center justify-between px-5 mx-auto h-24">
+        <Link to={"/"} className="text-7xl font-bold text-white font-RobotoCondensed">
           PetShop
         </Link>
-        <FiShoppingCart size={35} color="#121212" />
+        <Link to={"/cart"} className="relative">
+          <FiShoppingCart size={45} color="#121212" />
+          <span className="absolute -right-3 -top-5 bg-black rounded-full w-6 h-6 flex items-center justify-center text-sm font-bold text-white">
+            1
+          </span>
+        </Link>
       </nav>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        RobotoCondensed: ['Roboto Condensed']
+      }
+    },
   },
   plugins: [],
 }
