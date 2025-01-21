910ec1758291e42c3ab6f887e45aaff82e23eda1
BTFitti <bruno.fitti734@live.com>
2025-01-21 15:04:25 -0300

    Creating project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Cart } from "./pages/cart";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path:"/cart",
+    element: <Cart/>
+  }
+])
+export {router}
---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,7 @@
+export function Cart(){
+    return(
+        <div>
+            <h1>Pagina carrinho</h1>
+        </div>
+    )
+}
